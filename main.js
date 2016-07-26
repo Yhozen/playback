94e4a124340743c5aa413f511bb3d245d6a84945
Gabriel Pérez <gabriel@garox.org>
2016-07-25 23:31:32 -0300

    add visual error feedback

--- main.js
+++ main.js
@@ -7,32 +7,41 @@ titlebar.on('close', () => ipc.send('close'))
 
 titlebar.on('minimize', () => ipc.send('minimize'))
 
-titlebar.on('maximize',() => ipc.send('maximize'))
+titlebar.on('maximize', () => ipc.send('maximize'))
 
 titlebar.on('fullscreen', () => ipc.send('maximize'))
 
-//FETCH DATA
+// FETCH DATA
 const REQUEST_URL = 'http://192.168.1.33:5000/movies/1'
 
 fetch(REQUEST_URL)
   .then((response) => response.json())
   .then((responseData) => responseData.filter(isEnglish))
   .then((filtered) => filtered.map(startTorrent))
-  .catch((err) => console.error("THERE WAS AN ERROR: " + err))
+  .catch((err) => fetchError(err))
 
-//FUNCTIONS
+// FUNCTIONS
 var isEnglish = (movie) => movie.torrents.en
 
 var startTorrent = (movie) => {
   var movieDiv = document.createElement('div')
-  var movieImg = document.createElement("img")
+  var movieImg = document.createElement('img')
   movieImg.setAttribute('src', movie.images.poster)
   movieImg.setAttribute('class', 'responsive')
   movieDiv.appendChild(movieImg)
   movieDiv.setAttribute('class', 'movie-flex')
-  movieDiv.addEventListener("click", () => ipc.send('player', movie.torrents.en['720p'].url))
-  document.getElementById("container").appendChild(movieDiv)
+  movieDiv.addEventListener('click', () => ipc.send('player', movie.torrents.en['720p'].url))
+  document.getElementById('container').appendChild(movieDiv)
 }
 
-//SHOW APP
+var fetchError = (err) => {
+  var errorDiv = document.createElement('div')
+  errorDiv.setAttribute('class', 'error-div')
+  var errorText = document.createTextNode("Can't get the data from the API")
+  errorDiv.appendChild(errorText)
+  document.getElementById('container').appendChild(errorDiv)
+  console.error('THERE WAS AN ERROR: ' + err)
+}
+
+// SHOW APP
 ipc.send('readyMain')
